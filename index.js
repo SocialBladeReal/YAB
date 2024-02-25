92c15927c737fa331f15918b8b8bc5da3bedd897
Hunting378 <140703804+Hunting378@users.noreply.github.com>
2024-02-25 06:11:45 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,136 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
+
+setInterval(() => {
+  fetch(`https://axern.space/api/get?platform=youtube&type=channel&id=${id}`).then((res) => res.json()).then((data) => {
+                     document.getElementById("counter").innerHTML = data.estSubCount;
+
+    document.getElementById("userimg").src = data.snippet.thumbnails.high.url;
+    document.getElementById("userName").innerHTML = data.snippet.title;
+                  });
+      }, 2000);
+
+function search() {
+  const prompt = window.prompt("Enter channel name, ID, or URL.");
+  if (prompt)
+    fetch(
+      `https://livecounts.xyz/api/youtube-live-subscriber-count/search/${prompt}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        window.location.href = "?id=" + data.results[0].id;
+      });
+}
+function graphManager() {
+    console.log(data.estSubCount)
+    if (chart.series[0].points.length == 1500) chart.series[0].data[0].remove();
+    chart.series[0].addPoint([Date.now(), data.estSubCount])
+}
+const Interface = new LivecountseditInterface()
+window.onload = function () {
+    if (document.getElementById('tabs.0')) document.getElementById('tabs.0').click();
+}
+function submit() {
+    Interface.setValue()
+}
+
+function resetgraph() {
+
+    chart = new Highcharts.chart({
+        chart: {
+            renderTo: 'chart',
+            type: 'spline',
+            zoomType: 'x',
+            backgroundColor: 'transparent',
+            plotBorderColor: 'transparent'
+        },
+        title: {
+            text: ' '
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 500,
+            gridLineColor: textBright,
+            labels: {
+                style: {
+                    color: textBright
+                }
+            },
+            lineColor: lineColor,
+            minorGridLineColor: '#bdbdbd',
+            tickColor: lineColor,
+            title: {
+                style: {
+                    color: textBright
+                }
+            }
+        },
+        yAxis: {
+            enabled: false,
+        },
+        credits: {
+            enabled: true,
+            text: "Livecountsedit"
+        },
+        series: [{
+            showInLegend: false,
+            name: 'Subscribers',
+            marker: { enabled: false },
+            color: '#000000',
+            lineColor: "#b3382c"
+        }]
+    });
+
+}
+
+function random(min, max) {
+    return Math.random() * (max - min) + min
+}
+var textBright = "#bdbdbd"
+var lineColor = "#000000"
+var maxPoints = 20000;
+var chart = new Highcharts.chart({
+    chart: {
+        renderTo: 'chart',
+        type: 'spline',
+        zoomType: 'x',
+        backgroundColor: 'transparent',
+        plotBorderColor: 'transparent'
+    },
+    title: {
+        text: ' '
+    },
+    xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 500,
+        gridLineColor: textBright,
+        labels: {
+            style: {
+                color: textBright
+            }
+        },
+        lineColor: lineColor,
+        minorGridLineColor: '#bdbdbd',
+        tickColor: lineColor,
+        title: {
+            style: {
+                color: textBright
+            }
+        }
+    },
+    yAxis: {
+        visible: false,
+    },
+    credits: {
+        enabled: true,
+        text: "Livecountsedit"
+    },
+
+    series: [{
+        showInLegend: false,
+        name: 'Subscribers',
+        marker: { enabled: false },
+        color: '#b3382c',
+        lineColor: '#b3382c'
+    }]
+});
